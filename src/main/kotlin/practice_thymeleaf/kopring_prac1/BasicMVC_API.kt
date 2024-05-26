2af687b835f15d829a6f6604608e62fab16843f3
june <dydrkfl078@naver.com>
2024-05-26 14:10:32 +0900

    ::pencil:기초 MVC, API 학습

--- src/main/kotlin/practice_thymeleaf/kopring_prac1/BasicMVC_API.kt
+++ src/main/kotlin/practice_thymeleaf/kopring_prac1/BasicMVC_API.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class KopringPrac1Application
@@ -31,4 +32,39 @@ class TestClass {
 
 		return "test-mvc"
 	}
+
+	@GetMapping("test-api")
+	@ResponseBody
+	fun testApi(@RequestParam("name") name: String,@RequestParam("age") age: Int = 10): UserInform{
+		val user = UserInform()
+			.apply {
+				setName(name)
+				setAge(age)
+			}
+
+		return user
+	}
+}
+
+open class UserInform {
+	private lateinit var name : String
+	private var age : Int = 0
+
+	fun setName(name: String) {
+		this.name = name
+	}
+
+	fun getName() : String {
+		return name
+	}
+
+	fun setAge(age: Int) {
+		this.age = age
+	}
+
+	fun getAge() : Int {
+		return age
+	}
+
+
 }
