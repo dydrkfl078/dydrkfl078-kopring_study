d58f2504a97684e080705903968509d7d0ee3292
june <dydrkfl078@naver.com>
2024-05-30 02:21:43 +0900

    :card_file_box:Jdvanilla jdbc -> jdbc template 적용 - SpringConfig Repo Service 의존성 변경

--- src/main/kotlin/practice_thymeleaf/kopring_prac1/SpringConfig.kt
+++ src/main/kotlin/practice_thymeleaf/kopring_prac1/SpringConfig.kt
@@ -3,6 +3,7 @@ package practice_thymeleaf.kopring_prac1
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import practice_thymeleaf.kopring_prac1.repository.JdbcMemberRepo
+import practice_thymeleaf.kopring_prac1.repository.JdbcTemplateMemberRepo
 import practice_thymeleaf.kopring_prac1.repository.MemberRepo
 import practice_thymeleaf.kopring_prac1.repository.MemoryMemberRepo
 import practice_thymeleaf.kopring_prac1.service.MemberService
@@ -19,6 +20,7 @@ class SpringConfig(private val dataSource: DataSource) {
     @Bean
     fun MemberRepo(): MemberRepo {
 //        return MemoryMemberRepo()
-        return JdbcMemberRepo(dataSource)
+//        return JdbcMemberRepo(dataSource)
+        return JdbcTemplateMemberRepo(dataSource)
     }
 }
